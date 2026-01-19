a75e56a29787026430daf8cbd57b96a80fcc525c
Alex Ledger <alex@alexledger.net>
2026-01-19 10:16:15 -0800

    Add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+	// import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+	let { children } = $props<{ children: Snippet }>();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<!-- <svelte:head> -->
+	<!-- <link rel="icon" href={favicon} /> -->
+<!-- </svelte:head> -->
 
 {@render children()}
